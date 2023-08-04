c091b1b6fca9e738a06326faba13c07990fcfbfa
EBrown2124 <110208836+EBrown2124@users.noreply.github.com>
2023-08-03 22:57:20 -0700

    work on it 8-3-23

--- part1.js
+++ part1.js
@@ -1,66 +1,55 @@
-const rs = require('readline-sync');
-const start = rs.question('Press any key... ')
-const stirke = rs.question('Enter a location to strike ')
+// const rs = require('readline-sync');
 
-
-const run = (stirke) => {
-  const alpha = 'abcdefghij'.toUpperCase()
-  let boradsize = 3;
-  let shipLoc = [];
-
-  shipLoc
-  let grid = []
-  let stirkeLoc = stirke;
-  
-  const random = (size) => {
-    return Math.floor(Math.random() *size.length);
-  }
-
-  const creatGrid = (size) => {
-    for (let i = 0; i < size; i++) {
-      grid[i]=[];
-      for (let j = 0; j < size; j++) {
-        grid[i].push(`${alpha[i]}${j + 1}`)
-      }
-    }
-    return grid
-  }
-  creatGrid(boradsize)
-  
-  
-  const exist = (ship, shipLoc) => {
-    if(!shipLoc.includes(ship)) {
-      shipLoc.push(ship);
-      return true
-    }else{
-      return false
+// const start = rs.keyIn('Press any key... ')
+// const stirke = rs.question('Enter a location to strike ')
+const createGrid = () => {
+  const alpha = 'abcdefghij';
+  const borad = [];
+  for (let i = 0; i < 3; i++) {
+    borad[i]=[];
+    for (let j = 0; j < 3; j++) {
+      borad[i].push(`${alpha[i]}${j + 1}`);
     }
   }
-  
-  const place = () => {
-    const coords = grid.flat()
-    const loc = random(coords)
-    let ship = coords[loc]
-    return exist(ship,shipLoc)
-  }
-  place()
+  return borad
+}
 
-  const set = () => {
-    [1, 2].forEach((item) => place())
-  }
-  set()
-  
-  const hitMiss = () =>{
-    for (let i = 0; i < shipLoc.length; i++) {
-      shipLoc[i]
-      if(stirkeLoc === shipLoc[i]) {
-       console.log('hit');
-      }else{
-        console.log('miss')
-      }
-    }
-  }  
-  hitMiss(stirkeLoc)
-  }
 
-  console.log(run(stirke))
+const exist = (ship, shipLoc) => {
+  if(!shipLoc.includes(ship)) {
+    shipLoc.push(ship);
+    return true
+  }else{
+    return false
+  }
+}
+
+const rng = () =>{
+  const coords = grid.flat()
+  const loc = Math.floor(Math.random() * coords.length)
+  let ship = coords[loc]
+  return exist(ship,shipLoc)
+}
+
+const set = () => {
+  [1,2].forEach((item) => rng())
+}
+
+const shipCheck = (grid, location) => {
+  const spot = location.toLowerCase()
+  if (grid === spot) {
+    return 'hit';
+  }
+  return 'miss';
+}
+
+
+
+let grid = createGrid();
+let shipLoc = []
+let call = 'a1';
+let result = shipCheck(shipLoc, call );
+
+
+console.log(shipLoc);
+console.log(result);
